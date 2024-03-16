eff15a6f443ead968393780a1c141ef4ddec89de
MMHE <mominhasanemon@gmail.com>
2024-03-16 16:12:07 +0600

    Live test number one.

--- live_test_one/fruit.dart
+++ live_test_one/fruit.dart
@@ -2,19 +2,20 @@
 void main() {
   //A list, that contrain fruit details.
   List<Map<String, String>> fruit_menu = [
-    {'Name': 'Mango', 'Color': 'Green', 'Price': '20'},
-    {'Name': 'Banana', 'Color': 'Yellow', 'Price': '70'},
-    {'Name': 'Apple', 'Color': 'Red', 'Price': '50'}
+    {'Name': 'Mango', 'Color': 'Green', 'Price': '24.68'},
+    {'Name': 'Banana', 'Color': 'Yellow', 'Price': '12.56'},
+    {'Name': 'Apple', 'Color': 'Red', 'Price': '84.28'}
   ];
 
   //Function calling
   displayFruitDetails(fruit_menu);
-  applyPriceDiscount(fruit_menu, 33);
+  applyPriceDiscount(fruit_menu, 25);
 }
 
 //This function will fetch through a list, and print the output.
 void displayFruitDetails(List<Map<String, String>> item) {
   print('Regular price:');
+
   //for loop
   for (Map<String, String> fruit in item) {
     print('${fruit.toString()}\n');
@@ -26,9 +27,13 @@ void applyPriceDiscount(List<Map<String, String>> item, double percent) {
   print('You are getting $percent% discount:');
   //for loop
   for (Map<String, String> fruit in item) {
-    double orginalPrice = double.parse(fruit['Price'] ?? '0');
-    double discounntedPrice = (orginalPrice - ((orginalPrice / 100) * percent));
+    //Calculating the discount.
+    double discounntedPrice = ((double.parse(fruit['Price'] ?? '0')) -
+        (((double.parse(fruit['Price'] ?? '0')) / 100) * percent));
+
+    //applying the discount.
     fruit['Price'] = "${discounntedPrice.toStringAsFixed(2)}";
+
     print('${fruit.toString()}\n');
   }
 }
